38fe6772cef687f733e667f2a77208097077973a
Anton Afanasev <afanasev.anton@icloud.com>
2020-01-23 10:39:51 +0100

    add intremediate exercise 1

--- scripts.js
+++ scripts.js
@@ -8,3 +8,20 @@ var players=["Martin","Thomas","Peter","Mathias","Niki"];
 var playerAge=[12,14,3,5,67,88,33];
 console.log(players.slice(2,4));
 console.log("my name is "+players[3]+", i am "+playerAge[3]);
+
+var a = true;
+console.log(a);
+a=10<4;
+console.log(a);
+
+var b = 45;
+console.log(b);
+a = b-2;
+console.log(a);
+
+var cars=["Tesla","Audi","Renault","Volvo","Mazda","Fiat","Ferrari"];
+
+console.log(cars.sort());
+
+//document.getElementById("print").externalHTML = cars;
+
