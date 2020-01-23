42055ef62ba6de4c6d829defd9a71016a344414a
Anton Afanasev <afanasev.anton@icloud.com>
2020-01-23 10:15:33 +0100

    add files

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//document.write("my name is BART")
+console.log("help me, browser speaks with me");
+var name="Bart";
+console.log("My name is "+name);
+var age="25";
+console.log("I am "+age+" years old");
+var players=["Martin","Thomas","Peter","Mathias","Niki"];
+var playerAge=[12,14,3,5,67,88,33];
+console.log(players.slice(2,4));
+console.log("my name is "+players[3]+", i am "+playerAge[3]);
